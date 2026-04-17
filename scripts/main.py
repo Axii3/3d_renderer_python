380cc140e53e1eb7b6fcc735ffc1b3842d64be38
Finn Kitschke <finnkitschke@gmail.com>
2026-04-17 17:28:20 +0200

    Add the "3D" to the 3D renderer

--- scripts/main.py
+++ scripts/main.py
@@ -1,22 +1,49 @@
 import pygame
-from pygame import Vector2
+from pygame import Vector3
 import renderer
 from renderer import Vertex
 import window
 
 ### references
 
-### variables
+# --
 
-window.render.add_vertex(Vertex(Vector2(-50, 50)))
-window.render.add_vertex(Vertex(Vector2(50, 50)))
-window.render.add_vertex(Vertex(Vector2(50, -50)))
-#window.render.add_vertex(Vertex(Vector2(-40, -50)))
+### variables
+player_pos = Vector3(0, 0, 1)
+player_speed = 100.0
+
+
+window.render.add_vertex(Vertex(Vector3(-50, 50, 0)))
+window.render.add_vertex(Vertex(Vector3(50, 50, 0)))
+window.render.add_vertex(Vertex(Vector3(50, -50, 0)))
+window.render.add_vertex(Vertex(Vector3(-50, -50, 0)))
+window.render.add_vertex(Vertex(Vector3(-50, 50, 50)))
+window.render.add_vertex(Vertex(Vector3(50, 50, 50)))
+window.render.add_vertex(Vertex(Vector3(50, -50, 50)))
+window.render.add_vertex(Vertex(Vector3(-50, -50, 50)))
 
 while window.running:
+    window.deltatime = window.clock.tick(60) / 1000
+    deltatime = window.deltatime
+
     for event in window.pygame.event.get():
         if event.type == pygame.QUIT:
             window.running = False
+
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.z += player_speed * deltatime
+    if keys[pygame.K_s]:
+        player_pos.z -= player_speed * deltatime
+    if keys[pygame.K_a]:
+        player_pos.x -= player_speed * deltatime
+    if keys[pygame.K_d]:
+        player_pos.x += player_speed * deltatime
+    if keys[pygame.K_e]:
+        renderer.field_of_view += 1
+        print(renderer.field_of_view)
+
 
     window.screen.fill("#000000")
 
@@ -24,10 +51,12 @@ while window.running:
     window.render.draw_vertices()
 
     # update
-    renderer.camera_position.x += 0.1
+    renderer.camera_position.x = player_pos.x
+    renderer.camera_position.y = player_pos.y
+    renderer.camera_position.z = player_pos.z
 
     window.pygame.display.flip()
-    window.deltatime = window.clock.tick(60) / 1000
+
 
 
 window.pygame.quit()
--- scripts/renderer.py
+++ scripts/renderer.py
@@ -1,15 +1,17 @@
 
 import pygame
+from pygame import Vector3
+from pygame import Vector2
 
 pygame.init()
 
 ### references
 
 ### variables
-camera_position = pygame.Vector2(0, 0)
+camera_position = Vector3(0, 0, 1)
 window_width = 0
 window_height = 0
-
+field_of_view = 150
 
 
 class Renderer():
@@ -21,15 +23,29 @@ class Renderer():
 		for v in range(len(self.vertices)):
 			current_vertex = self.vertices[v]
 			current_vertex.draw(self.screen)
-			pygame.draw.line(self.screen, "#CCCCCC", current_vertex.screen_position, self.vertices[v-1].screen_position, 5)
+			#if (v>0):
+			pygame.draw.line(self.screen, "#CCCCCC", current_vertex.get_screen_space_pos(), self.vertices[v-1].get_screen_space_pos(), 5)
+
 
 	def add_vertex(self, vertex):
 		self.vertices.append(vertex)
 
 class Vertex():
 	def __init__(self, position):
-		self.position = position
-		self.screen_position = pygame.Vector2(window_width/2 + self.position.x, window_height/2 + self.position.y)
+		self.world_position = position
 		pass
 	def draw(self, screen):
-		pygame.draw.circle(screen, "#FFFFFF", (self.screen_position.x, self.screen_position.y), 10.0)
+		pos = self.get_screen_space_pos()
+		pygame.draw.circle(screen, "#FFFFFF", (pos.x, pos.y), 5.0)
+
+	def get_screen_space_pos(self):
+		vs_pos = self.get_view_space_pos()
+		ss_x = (vs_pos.x * field_of_view) / (vs_pos.z + field_of_view)
+		ss_y = (vs_pos.y * field_of_view) / (vs_pos.z + field_of_view)
+		# offset pivot from top left to center
+		offset_pos = Vector2(window_width/2 + ss_x, window_height/2 + ss_y)
+		return offset_pos
+
+	def get_view_space_pos(self):
+		view_pos = Vector3(self.world_position.x - camera_position.x, self.world_position.y - camera_position.y, self.world_position.z - camera_position.z)
+		return view_pos
