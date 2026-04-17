033b0f0d7acf5ee70b03bc25ec4f5a4d0cb96e24
Finn Kitschke <finnkitschke@gmail.com>
2026-04-17 20:30:03 +0200

    Add near plane culling

--- scripts/renderer.py
+++ scripts/renderer.py
@@ -12,7 +12,7 @@ camera_position = Vector3(0, 0, 0)
 window_width = 0
 window_height = 0
 field_of_view = 300
-#near_clipping_plane = -300
+near_clipping_plane = -50
 vertex_rendering = False
 
 class Renderer():
@@ -43,10 +43,11 @@ class Vertex():
 
 	def get_screen_space_pos(self):
 		vs_pos = self.get_view_space_pos()
-		if (vs_pos.z + field_of_view == 0):
-			vs_pos.z += 1
+		if (vs_pos.z + field_of_view < 0):
+			vs_pos.z = -field_of_view + 1
 		ss_x = (vs_pos.x * field_of_view) / (vs_pos.z + field_of_view)
 		ss_y = -(vs_pos.y * field_of_view) / (vs_pos.z + field_of_view)
+
 		# offset pivot from top left to center
 		offset_pos = Vector2(window_width/2 + ss_x, window_height/2 + ss_y)
 		return offset_pos
@@ -54,6 +55,12 @@ class Vertex():
 	def get_view_space_pos(self):
 		view_pos = Vector3(self.world_position.x - camera_position.x, self.world_position.y - camera_position.y, self.world_position.z - camera_position.z)
 		return view_pos
+
+	def on_screen(self):
+		if (self.get_view_space_pos().z + field_of_view < near_clipping_plane):
+			return False
+
+		return True
 
 class Triangle():
 	def __init__(self, screen, vertex1, vertex2, vertex3):
@@ -63,7 +70,9 @@ class Triangle():
 		self.vertex3 = vertex3
 
 	def draw(self, screen):
-		#if (self.vertex1.on_screen() | self.vertex2.on_screen() | self.vertex3.on_screen()):
+		if not (self.vertex1.on_screen() or self.vertex2.on_screen() or self.vertex3.on_screen()):
+			return
+
 		pygame.draw.line(self.screen, "#CCCCCC", self.vertex1.get_screen_space_pos(), self.vertex2.get_screen_space_pos(), 5)
 		pygame.draw.line(self.screen, "#CCCCCC", self.vertex2.get_screen_space_pos(), self.vertex3.get_screen_space_pos(), 5)
 		pygame.draw.line(self.screen, "#CCCCCC", self.vertex3.get_screen_space_pos(), self.vertex1.get_screen_space_pos(), 5)
